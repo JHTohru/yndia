bcfa8765b846c6b94a346cdd649869ba45676696
Tohru <jhtohru@gmail.com>
2018-10-10 13:59:39 -0300

    Ajustes

--- wp-content/themes/yndia/js/app/i18n/i18n.js
+++ wp-content/themes/yndia/js/app/i18n/i18n.js
@@ -1,9 +1,17 @@
 define({
     pt: {
         'navigation-tutorial': 'Use os botões ou as setas do teclado para navegar',
-        'try-extra-dry-title': 'Apenas 28<br>calorias',
         'try-extra-dry-text': 'Conheça também<br>a versão Extra Dry.<br>Clique aqui e saiba mais.',
+        'try-extra-dry-title': 'Apenas 28<br>calorias',
     },
-    en: { 'navigation-tutorial': 'Use the buttons or the arrow keys to navigate' },
-    it: { 'navigation-tutorial': 'Use the buttons or the arrow keys to navigate' },
+    en: {
+        'navigation-tutorial': 'Use the buttons or the arrow keys to navigate',
+        'try-extra-dry-text': 'Also try<br>the Extra Dry version too.<br>Click here to learn more.',
+        'try-extra-dry-title': 'Only 28<br>calories',
+    },
+    it: {
+        'navigation-tutorial': 'Utilizza le frecce della tastiera per navigare',
+        'try-extra-dry-text': 'Prova anche<br>la versione Extra dry.<br>Clicca qui per saperne di piu',
+        'try-extra-dry-title': 'Solo 28<br>calorie',
+    },
 });
